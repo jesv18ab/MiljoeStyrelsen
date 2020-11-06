ea0ed365aa4d036f56c066da21b416257b9c6487
my name <myemailid@domain.com>
2020-11-06 16:19:32 +0100

    Updated

--- App.js
+++ App.js
@@ -3,24 +3,36 @@ import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 const url = 'https://arpo-prod-api-app.azurewebsites.net/taxons/?searchText=&take=15&skip=0&notMatched=false&isDkTaxon=true&isDefaultTaxon=true&isMissingPhoto=false&speciesGroups=Fugle';
 
 export default class App extends React.Component {
-
+//Oprettelse af tre tomme state arrays til at hente data fra endpointet
   state = {
     birdsInfoList: [],
     withImageUrl: [],
     withoutImageUrl: []
   };
 
+  //Oprettelse af componentDidMount metode til at diktere de metoder, som skal kaldes, når komponenten mounter
   componentDidMount() {
     this.fetchData()
   }
 
+  //Denne metode står for at hente- og placere data fra endpointet
   fetchData = async() => {
+
+    // Fetch metoden efterspørger data fra et url og gemmer resultatet i response-variablen
     let response = await fetch(url);
+
+    //Result variablen er resultatet af deserialisering af json data
     const result = await response.json();
+
+    //Oprettelse af to arraya til at gemme data
     var withImageUrl =[];
     var withoutImageUrl =[];
+
+    //birdslisten indeholder al data fra url'et efter deserialisering
     const birdsList = result.items;
-    this.setState({birdsInfoList: birdsList});
+
+    //Denæst bruges map metoden til at gennemgå alle dataobjekter i hele listen , hvorefter der testes på om attributten imageUrl er en tom streng
+    //På baggrund af attributtens værdi gemmes data i den ene eller den anden liste
     birdsList.map((item, index) => {
       if (item.imageUrl === ""){
         withoutImageUrl.push(item)
@@ -29,20 +41,24 @@ export default class App extends React.Component {
     }
     });
 
-    this.setState({birdsInfoList: birdsList})
-    this.setState({withImageUrl: withImageUrl})
+//Slutteligt sættes de to statevariabler
+    this.setState({birdsInfoList: birdsList});
+    this.setState({withImageUrl: withImageUrl});
     this.setState({withoutImageUrl: withoutImageUrl})
-
   };
 
   render() {
-
+  //Her gemmes værdierne i to nye variabler, som printes ved brug af map metoden
     const withImageUrl= Object.values(this.state.withImageUrl);
-    const keyswith = Object.keys(this.state.birdsInfoList);
     const withoutImageUrl= Object.values(this.state.withoutImageUrl);
-    const keysWithout = Object.keys(this.state.birdsInfoList);
   return(
 <View style={styles.container}>
+  <Text
+      size={18}
+      style={styles.paragraph}
+  >
+    Fugle beskrivelser
+  </Text>
   <ScrollView style={styles.scrollView}>
   {withImageUrl.map((item, key) =>
       (
@@ -96,5 +112,11 @@ itemList: {
   scrollView: {
     backgroundColor: 'pink',
     marginHorizontal: 20,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 48,
+    fontWeight: 'bold',
+    textAlign: 'center',
   }
 });
